ebce795b199a710ebf934be0479150441f5e99d6
awhite727 <144245318+awhite727@users.noreply.github.com>
2023-10-24 00:25:41 -0500

    People class

---
+++ People.java
@@ -0,0 +1,49 @@
+public class People {
+
+    private String libraryNumber;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private double feesDue;
+    //constructor
+    public People(String libraryNumber, String firstName, String lastName, String phoneNumber) {
+        this.libraryNumber = libraryNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        feesDue = 0     //all new members start with a fee of 0
+    }
+    //getters for the attributes
+    public String getLibraryNumber() {
+        return libraryNumber;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public double getFeesDue() {
+        return feesDue;
+    }
+    //setters for the attributes
+    public void setLibraryNumber(String libraryNumber) {
+        this.libraryNumber = libraryNumber;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setFeesDue(double feesDue) {
+        this.feesDue = feesDue;
+    }
+    
+} 
