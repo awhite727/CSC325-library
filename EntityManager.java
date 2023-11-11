8c8b63e065959abe27ee13b74227e680d2f5b4f0
awhite727 <144245318+awhite727@users.noreply.github.com>
2023-11-11 01:59:27 -0600

    calculateFees now checks that the book is late before accruing fees

--- EntityManager.java
+++ EntityManager.java
@@ -3,20 +3,20 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class EntityManager {
-
+	// creates a Book object
 	public Book createBook (String title, String author, String ISBN, String genre, String copies) {
         // converts copies into an integer, and passes parameters into Book
 		int copyInt = Integer.parseInt(copies);
 		Book book = new Book(title, author, ISBN, genre, copyInt);
 		return book;
 	}
-	
+	// creates a People object
 	public People createPeople (String libraryNumber, String firstName, String lastName, String phoneNumber) {
         // passes parameters into People
 		People people = new People(libraryNumber, firstName, lastName, phoneNumber);
 		return people;
 	}
-	
+	// creates a Transaction object
 	public Transaction createTransaction (String ISBN, String libraryNumber) {
         // converts libraryNumber to an integer
 		int libraryNumberInt = Integer.parseInt(libraryNumber);
@@ -30,15 +30,17 @@ public class EntityManager {
 		Transaction transaction = new Transaction(ISBN, libraryNumberInt, dueDate);
 		return transaction;
 	}
-
+	// calculates fees due for a transaction
 	public double calculateFees (Transaction transaction) {
 		// gets dates
 		Calendar dueDate = transaction.getDueDate;
 		Calendar currDate = Calendar.getInstance();
-		// gets days between due date and current date
-		int days = (int) Duration.between(dueDate.toInstant(), currDate.toInstant()).toDays();
-		double fees = days*0.1;		// ten cent fee per day
+		double fees = 0.0;
+		if (dueDate.before(currDate)) {		// checks that the book is late
+			// gets days between due date and current date
+			int days = (int) Duration.between(dueDate.toInstant(), currDate.toInstant()).toDays();
+			double fees = days*0.1;		// ten cent fee per day
+		}
 		return fees;
 	}
-	
 }
